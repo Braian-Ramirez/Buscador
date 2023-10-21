5029efe64589dad0c1cab367f18bc5852c8dd2e7
Braian-Ramirez <braiannf.ramirezo@konradlorenz.edu.co>
2023-10-21 16:36:47 -0500

    implementamos el endpoint de merge sort

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import mock
 import json
-
+import random
+import string
 from nodo import Nodo
 
 app = FastAPI()
@@ -96,3 +97,51 @@ def buscar_numero_repetido(nums):
     if not resultado:
         print(json.dumps({"mensaje": "No se encontró ningún número repetido en la lista."}, ensure_ascii=False))
     return {}
+
+
+@app.post("/merge-sort")
+
+def generar_documentos():
+    # Generar una lista de 500 documentos ordenados alfabéticamente
+    documentos = []
+    for _ in range(500):
+        documento = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=10))
+        documentos.append(documento)
+    documentos.sort()
+    return documentos
+
+
+# Implementar el algoritmo de Merge Sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+    return arr
+# Ordenar la lista de documentos utilizando Merge Sort
+respuesta = merge_sort(generar_documentos())
+print(respuesta)
